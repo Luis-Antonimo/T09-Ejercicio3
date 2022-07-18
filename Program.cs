77663bb96858f6418c8621e0ea2e2ee0e631689c
Luis-Antonimo <luisantonimo@gmail.com>
2022-07-19 00:35:27 +0200

    ok

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace T09Ejercicio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Operaciones empleado = new Operaciones();
+            empleado.Suma();
+            empleado.Division();
+            empleado.Multiplicacion();
+            empleado.Resta();
+        }
+
+        class Operaciones
+        {
+            private double valorA;
+            private double valorB;
+
+            public Operaciones()
+            {
+                Console.WriteLine("Escriba el valor A:");
+                valorA = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Escriba el valor B:");
+                valorB = Convert.ToDouble(Console.ReadLine());
+            }
+
+            public void Suma()
+            {
+                Console.WriteLine("La suma de A y B es: " + (valorA + valorB));
+            }
+
+            public void Resta()
+            {
+                Console.WriteLine("La resta de A y B es: " + (valorA - valorB));
+            }
+
+            public void Division()
+            {
+                Console.WriteLine("La división de A y B es: " + (valorA / valorB));
+            }
+
+            public void Multiplicacion()
+            {
+                Console.WriteLine("La nultiplicación de A y B es: " + (valorA * valorB));
+            }
+        }
+    }
+}
